003d5c0264eb42b7dc0bcff60a1d2565b4d30f25
yljang <yljang@bu.edu>
2024-04-10 21:09:10 -0400

    Final Project Update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,10 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+$(function () {
+	var $select = $(".1-100");
+	for (i = 1; i <= 100; i++) {
+		$select.append($('<option></option>').val(i).html(i))
+	}
+});
