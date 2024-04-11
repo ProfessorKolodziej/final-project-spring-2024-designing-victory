ffd87c35138940b4682fd4dd474b1c38867586b4
yljang <yljang@bu.edu>
2024-04-10 22:48:52 -0400

    getting rid of js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,25 +6,11 @@
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
 
+/*
 $(function () {
 	var $select = $(".1-100");
 	for (i = 1; i <= 100; i++) {
 		$select.append($('<option></option>').val(i).html(i))
 	}
 });
-
-//I think I need more time to figure out the JS part of this coding to finalize the html so this is placeholder
-// Event listener for the reset button
-$("#reset").on("click", function () {
-	// Reset color picker to default value
-	$(".color-picker").val("#FF5F5F");
-
-	// Reset font picker to default value (if applicable)
-	// Reset font-family of paragraph to default (if applicable)
-
-	// Reset number select to default value
-	$(".1-100").val("1");
-
-	// Reset pattern selection to default (if applicable)
-	// Reset jersey preview image to default pattern (if applicable)
-});
+*/
